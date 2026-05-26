2a2a415bf0905325d2149ffd8643d9093c6b6507
n0tesss <nicolas.outhenin@laposte.net>
2026-05-26 16:21:29 +0200

    Ajout atelier_07.py

--- 00_Concepts/atelier_07.py
+++ 00_Concepts/atelier_07.py
@@ -7,11 +7,16 @@ import sys
 
 valeur = b"\x00\x00\x00\x2A"
 
-print(f"big-endian : {int.from_bytes(valeur, "big")}")
-print(f"little-endian : {int.from_bytes(valeur, "little")}")
+big = int.from_bytes(valeur, "big")
+little = int.from_bytes(valeur, "little")
 
-print(f"big-endian : {int.from_bytes(inverse, "big")}")
+inverse = valeur[::-1]
+inverse_big = int.from_bytes(inverse, "big")
+
+print(f"big-endian : {big}")
+print(f"little-endian : {little}")
+print(f"octets inversés puis big-endian : {inverse_big}")
 
 # Pourquoi les valeurs 2 et 3 sont identiques ? 
 
-# 
+# Lire des octets en little-endian revient à inverser leur ordre puis les lire en big-endian. 
