27b06cc1fb54c18eeedf35bdfa7c408467c93e41
n0tesss <nicolas.outhenin@laposte.net>
2026-05-26 12:02:04 +0200

    Ajout atelier_05.py

--- 00_Concepts/atelier_05.py
+++ 00_Concepts/atelier_05.py
@@ -2,11 +2,26 @@
 
 # Usage : python3 atelier_05.py
 
+import socket
+
 def recv_ligne(sock) -> bytes:
-    data = b""
-    while not data.endswith(b"\n"):
+    morceaux = []
+    while True:
         chunk = sock.recv(1)
         if not chunk:
             break
-        data += chunk
-    return data
+        if chunk == b'\n':
+            break
+        morceaux.append(chunk)
+    return b''.join(morceaux)
+
+s1, s2 = socket.socketpair()
+
+with s1, s2:
+    s1.sendall(b"bonjour\nle monde\n")
+    print(recv_ligne(s2))  
+    print(recv_ligne(s2))
+            
+# Question bonus : pourquoi est-ce inefficace en pratique ? Quelle structure de données permettrait d'optimiser (sans changer la sémantique) ?
+
+# 
