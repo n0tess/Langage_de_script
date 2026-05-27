d02705cdf5deea23106894095d1bbc51f449969b
n0tesss <nicolas.outhenin@laposte.net>
2026-05-27 11:10:53 +0200

    Ajout atelier_05.py SocketServer

--- network/03_Socketserver/atelier_05.py
+++ network/03_Socketserver/atelier_05.py
@@ -1,4 +1,32 @@
+#!/usr/bin/env python3
+
 # Atelier 5 - OUTHENIN Nicolas
 
-# Usage python3 atelier5.py 
+import socketserver
+import time
 
+socketserver.TCPServer.allow_reuse_address = True
+
+HOTE = "127.0.0.1"
+PORT = 8808
+
+class BonjourHandler(socketserver.StreamRequestHandler):
+      
+      def handle(self) -> None:
+        ligne = self.rfile.readline().rstrip(b"\n")
+
+        if not ligne:
+            return
+
+        print(f"    Reçu de {self.client_address} : {ligne!r}")
+        time.sleep(2)
+
+        self.wfile.write(b"Bonjour " + ligne + b".\n")
+
+class ServeurMultiClient(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+if __name__ == "__main__": 
+    with ServeurMultiClient((HOTE, PORT), BonjourHandler) as serveur:
+        print(f"Serveur en écoute sur {HOTE}:{PORT}...")
+        serveur.serve_forever()
